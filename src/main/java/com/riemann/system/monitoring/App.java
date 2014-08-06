d5e8d9a3f3c707e69652a5d2ea381ca55f0b4efe
SwanJumelle <jumelled@polytech.unice.fr>
2014-08-06 10:55:11 +0200

    boucle

--- src/main/java/com/riemann/system/monitoring/App.java
+++ src/main/java/com/riemann/system/monitoring/App.java
@@ -10,15 +10,16 @@ import com.aphyr.riemann.client.RiemannClient;
  */
 public class App 
 {
-	public static void main( String[] args ) throws IOException
+	public static void main( String[] args ) throws IOException, InterruptedException
 	{
 		RiemannClient c = RiemannClient.tcp("10.42.2.4", 5555);
-		for(int i=0;i<100;i++){
+		for(int i=0;i<20;i++){
 			c.connect();
 			c.event().service("fridge").state("running").metric(5.3).tags("appliance", "cold").send();
 
 			c.query("tagged \"cold\" and metric > 0"); // => List<Event>;
 			c.disconnect();
+			Thread.sleep(1000);
 		}
 	}
 }
