42e91c2f728ff72cdb1eb146c25c8c52249f77cb
SwanJumelle <jumelled@polytech.unice.fr>
2014-08-07 12:48:55 +0200

    test on servers

--- src/main/java/com/riemann/system/monitoring/App.java
+++ src/main/java/com/riemann/system/monitoring/App.java
@@ -44,12 +44,13 @@ public class App
 	{
 		RiemannCommunicator riemannCommunicator = new RiemannCommunicator("10.42.2.6");
 		DataSender dataSender = new DataSender(riemannCommunicator);
-		dataSender.printData();
-		try {
-			dataSender.sendData();
-		} catch (IOException | InterruptedException e) {
-			e.printStackTrace();
+		while(true){
+			//dataSender.printData();
+			try {
+				dataSender.sendData();
+			} catch (IOException | InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
-
 	}
 }
--- src/main/java/com/riemann/system/monitoring/DataSender.java
+++ src/main/java/com/riemann/system/monitoring/DataSender.java
@@ -15,7 +15,7 @@ import usage.MemData;
 import utils.Utils;
 
 public class DataSender {
-	
+
 	private static final int SEND_INTERVAL_TIME = 2000;
 	private RiemannCommunicator riemannCommunicator;
 
@@ -24,67 +24,65 @@ public class DataSender {
 	}
 
 	public void sendData() throws IOException, InterruptedException{
-		while(true){
-			ProcParser procP = new ProcParser(Utils.getPid());
-			ArrayList<CpuData> cpuDataList = new ArrayList<CpuData>();
-			MemData memData = procP.gatherMemoryUsage(Utils.getPid());
-			ArrayList<ArrayList<DiskData>> diskDataList = new ArrayList<ArrayList<DiskData>>();
-			for(int i=0;i<2;i++){
-				cpuDataList.add(procP.gatherCpuUsage());
-				diskDataList.add(procP.gatherDiskUsage());
-				try{
-					if(i%2 == 0)
-						Thread.sleep(SEND_INTERVAL_TIME);
-				}catch(InterruptedException e){
+		ProcParser procP = new ProcParser(Utils.getPid());
+		ArrayList<CpuData> cpuDataList = new ArrayList<CpuData>();
+		MemData memData = procP.gatherMemoryUsage(Utils.getPid());
+		ArrayList<ArrayList<DiskData>> diskDataList = new ArrayList<ArrayList<DiskData>>();
+		for(int i=0;i<2;i++){
+			cpuDataList.add(procP.gatherCpuUsage());
+			diskDataList.add(procP.gatherDiskUsage());
+			try{
+				if(i%2 == 0)
+					Thread.sleep(SEND_INTERVAL_TIME);
+			}catch(InterruptedException e){
 
-				}
 			}
-			riemannCommunicator.send("cpu_total_usage", Utils.totalCpuUsage(cpuDataList.get(0), cpuDataList.get(1)),"cpuUsage");
-			riemannCommunicator.send("mem_free", (memData.getMemFree()*100.0f)/memData.getMemTotal(),"mem_free");
-			riemannCommunicator.send("mem_buffers", (memData.getMemBuffers()*100.0f)/memData.getMemTotal(),"mem_buffers");
-			riemannCommunicator.send("mem_used", (memData.getMemUsed()*100.0f)/memData.getMemTotal(),"mem_used");
-			HashMap<String,ArrayList<Double>> diskUsage = Utils.getDiskStats(diskDataList.get(0),diskDataList.get(1));
-			Iterator<String> it = diskUsage.keySet().iterator();
-			while(it.hasNext()) {
-				String partitionName = it.next();
-				riemannCommunicator.send("disk_write_" + partitionName, diskUsage.get(partitionName).get(0), "disk_write_" + partitionName);
-				riemannCommunicator.send("disk_read_" + partitionName, diskUsage.get(partitionName).get(1), "disk_read_" + partitionName);
-			}
-			riemannCommunicator.send("loadavg", procP.gatherLoadAvg().getOneMinuteAvg(), "loadavg");
 		}
+		riemannCommunicator.send("cpu_total_usage", Utils.totalCpuUsage(cpuDataList.get(0), cpuDataList.get(1)),"cpuUsage");
+		riemannCommunicator.send("mem_free", (memData.getMemFree()*100.0f)/memData.getMemTotal(),"mem_free");
+		riemannCommunicator.send("mem_buffers", (memData.getMemBuffers()*100.0f)/memData.getMemTotal(),"mem_buffers");
+		riemannCommunicator.send("mem_used", (memData.getMemUsed()*100.0f)/memData.getMemTotal(),"mem_used");
+		HashMap<String,ArrayList<Double>> diskUsage = Utils.getDiskStats(diskDataList.get(0),diskDataList.get(1));
+		Iterator<String> it = diskUsage.keySet().iterator();
+		while(it.hasNext()) {
+			String partitionName = it.next();
+			riemannCommunicator.send("disk_write_" + partitionName, diskUsage.get(partitionName).get(0), "disk_write_" + partitionName);
+			riemannCommunicator.send("disk_read_" + partitionName, diskUsage.get(partitionName).get(1), "disk_read_" + partitionName);
+		}
+		riemannCommunicator.send("loadavg", procP.gatherLoadAvg().getOneMinuteAvg(), "loadavg");
+
 	}
 
 	public void printData(){
-		while(true){
-			Timestamp currentTimestamp = new java.sql.Timestamp(Calendar.getInstance().getTime().getTime());
-			ProcParser procP = new ProcParser(Utils.getPid());
-			ArrayList<CpuData> cpuDataList = new ArrayList<CpuData>();
-			MemData memData = procP.gatherMemoryUsage(Utils.getPid());
-			ArrayList<ArrayList<DiskData>> diskDataList = new ArrayList<ArrayList<DiskData>>();
-			for(int i=0;i<2;i++){
-				cpuDataList.add(procP.gatherCpuUsage());
-				diskDataList.add(procP.gatherDiskUsage());
-				try{
-					if(i%2 == 0)
-						Thread.sleep(SEND_INTERVAL_TIME);
-				}catch(InterruptedException e){
+		Timestamp currentTimestamp = new java.sql.Timestamp(Calendar.getInstance().getTime().getTime());
+		ProcParser procP = new ProcParser(Utils.getPid());
+		ArrayList<CpuData> cpuDataList = new ArrayList<CpuData>();
+		MemData memData = procP.gatherMemoryUsage(Utils.getPid());
+		ArrayList<ArrayList<DiskData>> diskDataList = new ArrayList<ArrayList<DiskData>>();
+		for(int i=0;i<2;i++){
+			cpuDataList.add(procP.gatherCpuUsage());
+			diskDataList.add(procP.gatherDiskUsage());
+			try{
+				if(i%2 == 0)
+					Thread.sleep(SEND_INTERVAL_TIME);
+			}catch(InterruptedException e){
 
-				}
 			}
-			System.out.println("***********************************\n     " + currentTimestamp + "\n***********************************");
-			System.out.println("cpu_total_usage " + Utils.totalCpuUsage(cpuDataList.get(0), cpuDataList.get(1)));
-			System.out.println("mem_free " + (memData.getMemFree()*100.0f)/memData.getMemTotal());
-			System.out.println("mem_buffers " +(memData.getMemBuffers()*100.0f)/memData.getMemTotal());
-			System.out.println("mem_used " + (memData.getMemUsed()*100.0f)/memData.getMemTotal());
-			HashMap<String,ArrayList<Double>> diskUsage = Utils.getDiskStats(diskDataList.get(0),diskDataList.get(1));
-			Iterator<String> it = diskUsage.keySet().iterator();
-			while(it.hasNext()) {
-				String partitionName = it.next();
-				System.out.println("disk_write_" + partitionName + " " + diskUsage.get(partitionName).get(0));
-				System.out.println("disk_read_" + partitionName + " " + diskUsage.get(partitionName).get(1));
-			}
-			System.out.println("loadavg " + procP.gatherLoadAvg().getOneMinuteAvg());
 		}
+		System.out.println("***********************************\n     " + currentTimestamp + "\n***********************************");
+		System.out.println("cpu_total_usage " + Utils.totalCpuUsage(cpuDataList.get(0), cpuDataList.get(1)));
+		System.out.println("mem_free " + (memData.getMemFree()*100.0f)/memData.getMemTotal());
+		System.out.println("mem_buffers " +(memData.getMemBuffers()*100.0f)/memData.getMemTotal());
+		System.out.println("mem_used " + (memData.getMemUsed()*100.0f)/memData.getMemTotal());
+		HashMap<String,ArrayList<Double>> diskUsage = Utils.getDiskStats(diskDataList.get(0),diskDataList.get(1));
+		Iterator<String> it = diskUsage.keySet().iterator();
+		while(it.hasNext()) {
+			String partitionName = it.next();
+			System.out.println("disk_write_" + partitionName + " " + diskUsage.get(partitionName).get(0));
+			System.out.println("disk_read_" + partitionName + " " + diskUsage.get(partitionName).get(1));
+		}
+		System.out.println("loadavg " + procP.gatherLoadAvg().getOneMinuteAvg());
+
 	}
 
 }
