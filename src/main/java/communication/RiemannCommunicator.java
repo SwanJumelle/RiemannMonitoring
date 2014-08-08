0756df76f5c75757beb368b069252c6ceafe101b
SwanJumelle <jumelled@polytech.unice.fr>
2014-08-08 13:50:35 +0200

    riemann-jmx

--- src/main/java/communication/RiemannCommunicator.java
+++ src/main/java/communication/RiemannCommunicator.java
@@ -7,13 +7,15 @@ import com.aphyr.riemann.client.RiemannClient;
 public class RiemannCommunicator {
 
 	private String address;
+	private int port;
 
-	public RiemannCommunicator(String address){
+	public RiemannCommunicator(String address, int port){
 		this.address = address;
+		this.port = port;
 	}
 
 	public void send(String fieldName, double metricValue, String tag) throws IOException, InterruptedException{
-		RiemannClient c = RiemannClient.tcp(address, 5555);
+		RiemannClient c = RiemannClient.tcp(address, port);
 		c.connect();
 		c.event().service(fieldName).state("running").metric(metricValue).tags("appliance", tag).send();
 		c.query("tagged \""+tag+"\" and metric > 0"); // => List<Event>;
